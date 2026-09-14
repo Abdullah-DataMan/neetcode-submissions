684a0c45df4e970d2eb18bcfbb2ed1db30cb964b
Abdullah Adel <abdullah.adel.g.h@gmail.com>
2026-09-14 16:32:22 +0300

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,18 @@
+import json
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = json.dumps(strs)
+        #print(encoded_string)
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+
+        if s == "":
+            strs = [""]
+            return strs
+        
+        strs = json.loads(s)
+        #print(strs)
+
+        return strs
